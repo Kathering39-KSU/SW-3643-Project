87b4618c2abf26246816eb6c790d97f9a63a64f0
Katherine Gibson <kgibso39@students.kennesaw.edu>
2024-02-13 15:53:11 -0500

    CurrentlyProgramming
    Errors may occur

--- Calculator/Calculator/CalculatorEngine.cs
+++ Calculator/Calculator/CalculatorEngine.cs
@@ -1,9 +1,42 @@
-﻿namespace Calculator;
+﻿using System.Diagnostics;
+
+namespace Calculator;
 
 public class CalculatorEngine
 {
-    public static double CalculateWebInputs(int inputA, int inputB)
+    public static double CalculateWebInputs(double inputA, double inputB, int mathFunction)
     {
-        return 5.0;
+        double functionResult = 0.0;
+        switch (mathFunction)
+        {
+            case 1:
+                functionResult = (inputA + inputB);
+                break;
+            case 2:
+                break;
+            case 3:
+                break;
+            case 4:
+                break;
+            case 5:
+                break;
+            case 6:
+                break;
+            case 7:
+                break;
+            case 8:
+                break;
+            case 9:
+                break;
+            case 10:
+                break;
+            case 11:
+                break;
+            case 12:
+                break;
+            case 13:
+                break;
+        }
+        return functionResult;
     }
 }
